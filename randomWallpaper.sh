175acd30abb30f9a96d9b3a864fbd617291c5372
Shahabaz Bagwan <cool.shahabaz@gmail.com>
2020-03-18 12:32:40 +0100

    added config files
    added the config files I use with i3wm and for application of wallpaper on dual monitor setup

---
+++ randomWallpaper.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Directory of wallpapers:
+DIR="$HOME/Dropbox/wallpapers/"
+
+# Choose a random wallpaper from files in that directory:
+FILES=($DIR/*)
+WALLPAPER=`printf "%s\n" "${FILES[RANDOM % ${#FILES[@]}]}"`
+WALLPAPER1=`printf "%s\n" "${FILES[RANDOM % ${#FILES[@]}]}"`
+# Set as desktop background:
+#nitrogen --set-scaled "$WALLPAPER"
+feh --bg-scale "$WALLPAPER" --bg-scale "$WALLPAPER1"
