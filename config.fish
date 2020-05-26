8908f77b69c442e372733ee16b3b2e0201615337
Shahabaz <cool.shahabaz@gmail.com>
2020-05-26 10:46:12 +0200

    added fish config

---
+++ config.fish
@@ -0,0 +1,12 @@
+# aliases 
+alias la 'ls -A'
+alias rm 'rm -vi'
+alias mv 'mv -vi'
+alias cp 'cp -v'
+alias r 'ranger'
+alias v 'vim'
+alias l 'ls'
+# theme settings usign Oh-My-Fish
+set -g theme_color_scheme light
+set -g theme_date_timezone Europe/Berlin
+
