ad70abf5b24a74bab07aa4338d75c654487d8db8
shahabaz <cool.shahabaz@gmail.com>
2020-05-26 11:34:38 +0200

    made correct gaps installer

--- i3gaps.sh
+++ i3gaps.sh
@@ -1,4 +1,16 @@
-cd ~/tmp
+#!/bin/bash
+
+sudo apt install -y libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev automake ranger vim vim-gtk python3-pip rofi mpv scrot thunar htop arandr zathura fish rxvt-unicode lightdm i3lock-fancy curl
+
+curl -L https://get.oh-my.fish | fish
+
+omf install bobthefish
+
+pip install --user bumblebee-status
+
+sudo pip3 install pywal
+
+cd /tmp
 git clone https://www.github.com/Airblader/i3 i3-gaps
 cd i3-gaps
 git checkout gaps && git pull
