8d1064d4bcf3cf38b9fe3e2af003db7357fb09aa
shahabaz <cool.shahabaz@gmail.com>
2020-06-03 12:31:13 +0200

    better install script

--- i3gaps.sh
+++ i3gaps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt install -y libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev automake ranger vim vim-gtk python3-pip rofi mpv scrot thunar htop arandr zathura fish rxvt-unicode lightdm i3lock-fancy curl lxappearance papirus-icon-theme numix-gtk-theme breeze-cursor-theme
+sudo apt install -y libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev automake ranger vim vim-gtk python3-pip rofi mpv scrot thunar htop arandr zathura fish rxvt-unicode lightdm i3lock-fancy curl lxappearance papirus-icon-theme numix-gtk-theme breeze-cursor-theme xclip
 
 curl -L https://get.oh-my.fish | fish
 
