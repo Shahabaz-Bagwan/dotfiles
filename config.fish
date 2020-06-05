677f5bbcb19bb10caaa652d9012e64c2cbf65787
shahabaz <cool.shahabaz@gmail.com>
2020-06-05 14:10:45 +0200

    some update and new monitors script

--- config.fish
+++ config.fish
@@ -1,11 +1,9 @@
 # aliases 
 alias la '/home/shahabaz/.cargo/bin/exa -al --color=always --group-directories-first'
-alias rm 'rm -vi'
+alias rm 'rm -vI'
 alias mv 'mv -vi'
 alias cp 'cp -v'
 alias r 'ranger'
-alias v 'vim'
-alias l 'ls'
 # theme settings usign Oh-My-Fish
 set -g theme_color_scheme light
 set -g theme_date_timezone Europe/Berlin
---
+++ monitors.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+EXTERNAL_OUTPUT="DP-1-1-1"
+EXTERNAL_OUTPUT2="DP-1-1-2"
+INTERNAL_OUTPUT="eDP-1-1"
+
+# if we don't have a file, start at zero
+if [ ! -f "/tmp/monitor_mode.dat" ] ; then
+  monitor_mode="all"
+
+# otherwise read the value from the file
+else
+  monitor_mode=`cat /tmp/monitor_mode.dat`
+fi
+
+if [ $monitor_mode = "all" ]; then
+        monitor_mode="EXTERNAL"
+        xrandr --output $INTERNAL_OUTPUT --off --output $EXTERNAL_OUTPUT --auto --primary --output $EXTERNAL_OUTPUT2 --auto --right-of $EXTERNAL_OUTPUT
+elif [ $monitor_mode = "EXTERNAL" ]; then
+        monitor_mode="INTERNAL"
+        xrandr --output $INTERNAL_OUTPUT --auto --primary --output $EXTERNAL_OUTPUT --off --output $EXTERNAL_OUTPUT2 --off
+elif [ $monitor_mode = "INTERNAL" ]; then
+        monitor_mode="CLONES"
+        xrandr --output $INTERNAL_OUTPUT --auto --primary --output $EXTERNAL_OUTPUT --auto --same-as $INTERNAL_OUTPUT
+else
+        monitor_mode="all"
+        xrandr --output $INTERNAL_OUTPUT --auto --primary --output $EXTERNAL_OUTPUT2 --auto --left-of $INTERNAL_OUTPUT --output $EXTERNAL_OUTPUT --auto --left-of $EXTERNAL_OUTPUT2
+fi
+echo "${monitor_mode}" > /tmp/monitor_mode.dat
