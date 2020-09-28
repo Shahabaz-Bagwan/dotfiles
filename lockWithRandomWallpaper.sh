3661fffb162332a2559e22fb675b55f2af1ad46e
shahabaz <cool.shahabaz@gmail.com>
2020-09-28 11:28:41 +0200

    added fonts, changed chat screen config in i3, locking set to correct width

--- lockWithRandomWallpaper.sh
+++ lockWithRandomWallpaper.sh
@@ -3,7 +3,8 @@
 # Directory of wallpapers:
 DIR="$HOME/Pictures/wallpapers"
 # Choose a random wallpaper from files in that directory:
-FILES=($DIR/*.png)
+FILES=($DIR/*)
 WALLPAPER=`printf "%s\n" "${FILES[RANDOM % ${#FILES[@]}]}"`
 # Set as desktop background:
-i3lock -ti "$WALLPAPER" 
+magick convert -geometry 1920x $WALLPAPER /tmp/lockImg.png 
+i3lock -ti /tmp/lockImg.png
