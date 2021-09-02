9d0a8cd9ee861a35819f51227765ad60b1df8ebc
Shahabaz Bagwan <4272320+Shahabaz-Bagwan@users.noreply.github.com>
2021-09-02 10:44:41 +0200

    update wordlist.sh

--- scripts/wordlist.sh
+++ scripts/wordlist.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 # prompt for adding a word
+
 word=$(rofi -dmenu -p "Add a word ")
 
 # Append word in a document
-echo "$word: " >> $HOME/repos/dotfiles/wordlist/Deutsch.txt
-st -e w3m de.wiktionary.org/wiki/$word
-
+if [ -z "$word" ]; then
+  exit 0;
+else
+  echo "$word: " >> $HOME/repos/dotfiles/wordlist/Deutsch.txt
+  st -e w3m de.wiktionary.org/wiki/$word
+fi 
 exit 0;
