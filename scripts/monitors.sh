00eec164104b50753a21b432732c6d045b5871a6
Shahabaz Bagwan <4272320+Shahabaz-Bagwan@users.noreply.github.com>
2025-02-19 08:26:53 +0100

    make external as default mode in monitor selection

--- scripts/monitors.sh
+++ scripts/monitors.sh
@@ -19,12 +19,7 @@ else
   monitor_mode=`cat /tmp/monitor_mode.dat`
 fi
 
-if [ $monitor_mode = "all" ]; then
-        monitor_mode="EXTERNAL"
-        xrandr --output $INTERNAL_OUTPUT --off --output $EXTERNAL_OUTPUT2 --auto --primary \
-               --output $EXTERNAL_OUTPUT --rotate right --auto \
-               --right-of $EXTERNAL_OUTPUT2
-elif [ $monitor_mode = "EXTERNAL" ]; then
+if [ $monitor_mode = "EXTERNAL" ]; then
         monitor_mode="INTERNAL"
         xrandr --output $INTERNAL_OUTPUT --auto --primary --output $EXTERNAL_OUTPUT \
                --off --output $EXTERNAL_OUTPUT2 --off
@@ -32,11 +27,16 @@ elif [ $monitor_mode = "INTERNAL" ]; then
         monitor_mode="CLONES"
         xrandr --output $INTERNAL_OUTPUT --auto --primary --output $EXTERNAL_OUTPUT2 \
                --auto --same-as $INTERNAL_OUTPUT
-else
-        monitor_mode="all"
+elif [ $monitor_mode = "CLONES" ]; then
+        monitor_mode="ALL"
         xrandr --output $INTERNAL_OUTPUT --auto --primary --output $EXTERNAL_OUTPUT2 \
           --auto --right-of $INTERNAL_OUTPUT --output $EXTERNAL_OUTPUT --rotate right \
           --auto --right-of $EXTERNAL_OUTPUT2
+else
+        monitor_mode="EXTERNAL"
+        xrandr --output $INTERNAL_OUTPUT --off --output $EXTERNAL_OUTPUT2 --auto --primary \
+               --output $EXTERNAL_OUTPUT --rotate right --auto \
+               --right-of $EXTERNAL_OUTPUT2
 fi
 echo "${monitor_mode}" > /tmp/monitor_mode.dat
 
